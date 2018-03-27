e53c74be3b822894deeb283ac8c2565de38f8362
Juan Pleitez <juanpc13lolol@gmail.com>
2018-03-27 15:14:46 -0600

    Metodos GET POST PUT DELETE implementados en MarcaRest

--- src/main/java/sv/edu/uesocc/ingenieria/tpi2018/mantenimiento/boundary/MarcaRest.java
+++ src/main/java/sv/edu/uesocc/ingenieria/tpi2018/mantenimiento/boundary/MarcaRest.java
@@ -11,9 +11,15 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.EJB;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import sv.edu.uesocc.ingenieria.tpi2018.mantenimiento.controller.MarcaFacadeLocal;
 import sv.edu.uesocc.ingenieria.tpi2018.mantenimiento.entity.Marca;
@@ -23,24 +29,136 @@ import sv.edu.uesocc.ingenieria.tpi2018.mantenimiento.entity.Marca;
  * @author jcpleitez
  */
 @Path("marca")
-public class MarcaRest implements Serializable{
-    
+public class MarcaRest implements Serializable {
+
     @EJB
     private MarcaFacadeLocal mfl;
-    
+
     @GET
-    @Produces({MediaType.APPLICATION_JSON+"; charset=utf-8"})
-    public List<Marca> findAll(){
-        if(mfl != null){
+    @Path("all")
+    @Produces({MediaType.APPLICATION_JSON + "; charset=utf-8"})
+    public List<Marca> findAll() {
+        if (mfl != null) {
             List<Marca> list = new ArrayList<>();
             try {
                 list = mfl.findAll();
             } catch (Exception e) {
                 Logger.getLogger(getClass().getName()).log(Level.SEVERE, e.getMessage(), e);
             }
-            return list;            
+            return list;
         }
-        return new ArrayList<>();
+        return null;
     }
     
+    @GET
+    @Produces({MediaType.APPLICATION_JSON + "; charset=utf-8"})
+    public List<Marca> findRange(
+            @DefaultValue("0") @QueryParam("first") int first,
+            @DefaultValue("5") @QueryParam("pagesize") int pageSize
+    ) {        
+        if (mfl != null) {
+            try {
+                List<Marca> list = null;
+                list = mfl.findRange(first, pageSize);
+                return list;
+            } catch (Exception e) {
+                Logger.getLogger(getClass().getName()).log(Level.SEVERE, e.getMessage(), e);
+            }
+        }
+        return null;
+    }
+
+    @GET
+    @Path("{idmarca}")
+    @Produces({MediaType.APPLICATION_JSON + "; charset=utf-8"})
+    public Marca findById(
+            @PathParam("idmarca") Integer id
+    ) {
+        if (mfl != null) {
+            Marca reg = null;
+            try {
+                reg = mfl.find(id);
+            } catch (Exception e) {
+                Logger.getLogger(getClass().getName()).log(Level.SEVERE, e.getMessage(), e);
+            }
+            return reg;
+        }
+        return null;
+    }
+
+    @GET
+    @Path("count")
+    @Produces({MediaType.TEXT_PLAIN})
+    public Integer count() {
+        try {
+            if (mfl != null) {
+                return mfl.count();
+            }
+
+        } catch (Exception e) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, e.getMessage(), e);
+        }
+        return null;
+    }
+
+    @DELETE
+    @Path("{id}")
+    @Produces({MediaType.APPLICATION_JSON + "; charset=utf-8"})
+    public Marca delete(
+            @PathParam("id") Integer id
+    ) {
+        try {
+            Marca reg = null;
+            if (mfl != null) {
+                reg = mfl.find(id);
+                if(reg != null){
+                    mfl.remove(reg);
+                }
+                return reg;
+            }
+        } catch (Exception e) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, e.getMessage(), e);
+        }
+        return null;
+    }
+    
+    @POST
+    @Produces({MediaType.APPLICATION_JSON+"; charset=utf-8"})
+    public Marca create(Marca registro){
+        if (registro != null && registro.getIdMarca()== null) {
+            try {
+                if (mfl != null) {
+                    Marca reg = mfl.crear(registro);
+                    if (reg != null) {
+                        return reg;
+                    }
+                }
+            } catch (Exception e) {
+                Logger.getLogger(getClass().getName()).log(Level.SEVERE, e.getMessage(), e);
+            }
+        }
+        return null;
+    }
+
+    @PUT    
+    @Produces({MediaType.APPLICATION_JSON + "; charset=utf-8"})
+    public Marca edit(Marca reg) {        
+        if (mfl != null) {
+            if (reg.getIdMarca() != null) {
+                //Verificar que exista ese registro
+                try {
+                    Marca regVerificado = mfl.find(reg.getIdMarca());
+                    if (regVerificado != null) {
+                        if (mfl.edit(reg)) {
+                            return mfl.find(reg.getIdMarca());
+                        }
+                    }
+                } catch (Exception e) {
+                    Logger.getLogger(getClass().getName()).log(Level.SEVERE, e.getMessage(), e);
+                }
+            }
+        }
+        return null;
+    }
+
 }
