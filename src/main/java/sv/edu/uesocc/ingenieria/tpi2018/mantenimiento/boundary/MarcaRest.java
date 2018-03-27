8493a64d00a936b29a06dae7abc1ea46b98cef88
Juan Pleitez <juanpc13lolol@gmail.com>
2018-03-27 12:15:39 -0600

    Agregando metodo findAll en MarcaRest para el web service

---
+++ src/main/java/sv/edu/uesocc/ingenieria/tpi2018/mantenimiento/boundary/MarcaRest.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sv.edu.uesocc.ingenieria.tpi2018.mantenimiento.boundary;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.EJB;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import sv.edu.uesocc.ingenieria.tpi2018.mantenimiento.controller.MarcaFacadeLocal;
+import sv.edu.uesocc.ingenieria.tpi2018.mantenimiento.entity.Marca;
+
+/**
+ *
+ * @author jcpleitez
+ */
+public class MarcaRest implements Serializable{
+    
+    @EJB
+    private MarcaFacadeLocal mfl;
+    
+    @GET
+    @Produces({MediaType.APPLICATION_JSON+"; charset=utf-8"})
+    public List<Marca> findAll(){
+        if(mfl != null){
+            List<Marca> list = new ArrayList<>();
+            try {
+                list = mfl.findAll();
+            } catch (Exception e) {
+                Logger.getLogger(getClass().getName()).log(Level.SEVERE, e.getMessage(), e);
+            }
+            return list;            
+        }
+        return new ArrayList<>();
+    }
+    
+}
