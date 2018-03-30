d3f891563fc4341948c062c3adca3189b5f1f01b
Diana Magaña <mb15004@ues.edu.sv>
2018-03-29 18:04:46 -0600

    Agregando el resource de Procedimiento

--- src/main/java/sv/edu/uesocc/ingenieria/tpi2018/mantenimiento/boundary/ApplicationConfig.java
+++ src/main/java/sv/edu/uesocc/ingenieria/tpi2018/mantenimiento/boundary/ApplicationConfig.java
@@ -45,6 +45,7 @@ public class ApplicationConfig extends Application {
         resources.add(sv.edu.uesocc.ingenieria.tpi2018.mantenimiento.boundary.ParteResource.class);
         resources.add(sv.edu.uesocc.ingenieria.tpi2018.mantenimiento.boundary.PasoResources.class);
         resources.add(sv.edu.uesocc.ingenieria.tpi2018.mantenimiento.boundary.PrioridadRest.class);
+        resources.add(sv.edu.uesocc.ingenieria.tpi2018.mantenimiento.boundary.ProcedimientoResource.class);
         resources.add(sv.edu.uesocc.ingenieria.tpi2018.mantenimiento.boundary.ResponsableResource.class);
         resources.add(sv.edu.uesocc.ingenieria.tpi2018.mantenimiento.boundary.SolicitudResources.class);
         resources.add(sv.edu.uesocc.ingenieria.tpi2018.mantenimiento.boundary.SubTipoMantenimientoResources.class);
