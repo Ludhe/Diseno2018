4a650f9ca3973036c84bab87344baac9c56f7562
Diana Magaña <mb15004@ues.edu.sv>
2018-04-02 19:41:33 -0600

    Trabajando en el test de TestEJB

--- src/test/java/sv/edu/uesocc/ingenieria/tpi2018/mantenimiento/entity/MarcaTestEJb.java
+++ src/test/java/sv/edu/uesocc/ingenieria/tpi2018/mantenimiento/entity/MarcaTestEJb.java
@@ -5,10 +5,49 @@
  */
 package sv.edu.uesocc.ingenieria.tpi2018.mantenimiento.entity;
 
+import org.jboss.arquillian.core.api.annotation.Inject;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import sv.edu.uesocc.ingenieria.tpi2018.mantenimiento.controller.MarcaFacade;
+
 /**
  *
  * @author rcarlos
  */
+
+@RunWith(Arquillian.class)
 public class MarcaTestEJb {
-    
+
+    @Inject
+    private MarcaFacade mf;
+
+ 
+
+    @Deployment
+    public static Archive<?> createDeployment() {
+
+        return ShrinkWrap.create(WebArchive.class, "test.war")
+                .addPackage(MarcaFacade.class.getPackage())
+                .addAsManifestResource("test-persistence.xml", "META-INF/persistence.xml")
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+
+    }
+
+ 
+
+    @Test
+
+    public void testCanPersistUserObject() {
+
+        Assert.assertTrue(true);    
+
+    }
 }
