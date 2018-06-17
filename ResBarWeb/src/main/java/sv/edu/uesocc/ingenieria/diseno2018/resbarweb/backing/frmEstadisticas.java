e85867f565329e0fbda2b80c75e2c6244a3749c7
andreacuellar2 <cuellarandrea@gmail.com>
2018-06-17 17:28:25 -0600

    Estadisticas componentes

--- ResBarWeb/src/main/java/sv/edu/uesocc/ingenieria/diseno2018/resbarweb/backing/frmEstadisticas.java
+++ ResBarWeb/src/main/java/sv/edu/uesocc/ingenieria/diseno2018/resbarweb/backing/frmEstadisticas.java
@@ -8,8 +8,10 @@ package sv.edu.uesocc.ingenieria.diseno2018.resbarweb.backing;
 import java.io.Serializable;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
@@ -17,8 +19,6 @@ import javax.faces.context.FacesContext;
 import org.primefaces.event.SelectEvent;
 import sv.edu.diseno.acceso.ManejadorOrden;
 import sv.edu.diseno.definiciones.Orden;
-import sv.edu.diseno.excepciones.ErrorAplicacion;
-
 /**
  *
  * @author andrea
@@ -30,33 +30,61 @@ public class frmEstadisticas implements Serializable {
     ManejadorOrden manejadorOrden;
     private List<Orden> historico;
     private Date date1;
+    private Date date2;
+    private Date date3;
     private Date fecha1;
     private Date fecha2;
+    private String seleccionado = "";
+    private List<String> opciones;
     String fechaSeleccionada;
+    
+    @PostConstruct
+    public void init() {
+        opciones = new ArrayList<>();
+        opciones.add("Por dia");
+        opciones.add("Por periodo");
+    }  
 
     public void onDateSelect(SelectEvent event) throws ParseException {
-
         FacesContext facesContext = FacesContext.getCurrentInstance();
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
         facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Date Selected", dateFormat.format(event.getObject())));
         fechaSeleccionada = dateFormat.format(event.getObject());
         fecha1 = dateFormat.parse(fechaSeleccionada);
-        System.out.println("FECHA1=" + fecha1);
         fecha2 = dateFormat.parse(fechaSeleccionada);
         fecha2.setHours(23);
         fecha2.setMinutes(59);
         fecha2.setSeconds(59);
-        System.out.println("fecha2=" + fecha2);
         historico = manejadorOrden.ObtenerVentas(fecha1, fecha2);
-        System.out.println(historico);
+        
     }
-
+    public  void imprimir(){
+        System.out.println(seleccionado);
+    }
+        
+    //ALL GETTERS AND SETTERS
     public Date getDate1() {
         return date1;
     }
 
     public void setDate1(Date date1) {
         this.date1 = date1;
+    }
+     
+    public Date getDate2() {
+        return date2;
+    }
+
+    public void setDate2(Date date2) {
+        this.date2 = date2;
+    }
+
+    public Date getDate3() {
+        return date3;
+    }
+
+    public void setDate3(Date date3) {
+        this.date3 = date3;
     }
 
     public List<Orden> getHistorico() {
@@ -66,5 +94,20 @@ public class frmEstadisticas implements Serializable {
     public void setHistorico(List<Orden> historico) {
         this.historico = historico;
     }
+    
+     public String getSeleccionado() {
+        return seleccionado;
+    }
 
+    public void setSeleccionado(String seleccionado) {
+        this.seleccionado = seleccionado;
+    }
+        
+    public List<String> getOpciones() {
+        return opciones;
+    }
+
+    public void setOpciones(List<String> opciones) {
+        this.opciones = opciones;
+    }
 }
