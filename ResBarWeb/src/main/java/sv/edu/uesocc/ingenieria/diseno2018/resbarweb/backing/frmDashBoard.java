f5a8dc7439fbf2cbdbe425c56aa5a29adaa92e2a
juanpc13 <juanpc13lolol@gmail.com>
2018-06-19 00:29:03 -0600

    Poniendo los dialogs separados y con forms

--- ResBarWeb/src/main/java/sv/edu/uesocc/ingenieria/diseno2018/resbarweb/backing/frmDashBoard.java
+++ ResBarWeb/src/main/java/sv/edu/uesocc/ingenieria/diseno2018/resbarweb/backing/frmDashBoard.java
@@ -6,28 +6,15 @@
 package sv.edu.uesocc.ingenieria.diseno2018.resbarweb.backing;
 
 import java.io.Serializable;
-import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
-import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-import javax.faces.context.FacesContext;
 import org.primefaces.context.RequestContext;
-import org.primefaces.event.SelectEvent;
-import org.primefaces.model.menu.DefaultMenuItem;
-import org.primefaces.model.menu.DefaultMenuModel;
-import org.primefaces.model.menu.DefaultSubMenu;
-import org.primefaces.model.menu.MenuModel;
-import sv.edu.diseno.acceso.ManejadorCategorias;
 import sv.edu.diseno.acceso.ManejadorOrden;
-import sv.edu.diseno.definiciones.Categoria;
 import sv.edu.diseno.definiciones.DetalleOrden;
-import sv.edu.diseno.definiciones.DetalleOrdenPK;
 import sv.edu.diseno.definiciones.Orden;
-import sv.edu.diseno.definiciones.Producto;
-import sv.edu.diseno.excepciones.ErrorAplicacion;
-import sv.edu.uesocc.ingenieria.diseno2018.resbarweb.ticket.NuevoTicket;
 
 @ManagedBean(name = "frmDashboard")
 @ViewScoped
@@ -36,156 +23,53 @@ public class frmDashBoard implements Serializable {
     ManejadorOrden manejadorOrden;
     private List<Orden> ordenes;
     private Orden selectedOrden;
-    private List<DetalleOrden> detalleOrden;
-    ManejadorCategorias manejadorCategorias;
-    private List<Categoria> categorias;
-    private Categoria selectedCategoria;
-    private Producto selectedProducto;
-    private int cantidadSelectedProducto = 1;
-    private MenuModel model;
-    NuevoTicket ticket = new NuevoTicket();
+    private DetalleOrden selectedDetalleOrden;
 
+    
+    //Metodos De la Importantes del FRM
     @PostConstruct
     public void init() {
-        model = new DefaultMenuModel();
-        categorias = manejadorCategorias.Obtener(true);
-        DefaultSubMenu firstSubmenu = new DefaultSubMenu("CATEGORÍAS");
-        for (int i = 0; i < categorias.size(); i++) {
-            DefaultMenuItem item = new DefaultMenuItem(categorias.get(i).nombre);
-            item.setIcon("ui-icon-arrowthick-1-e");
-            //item.setCommand("#{administrar.setIdCategoria("+i+")}");
-            item.setCommand("#{frmDashboard.generarSelectedCateogia(" + categorias.get(i).getIdCategoria() + ")}");
-            item.setUpdate(":form:agregarProductosPanel");
-            firstSubmenu.addElement(item);
-        }
-        model.addElement(firstSubmenu);
+        ordenes = new ArrayList<>();
+        ordenes = manejadorOrden.ObtenerActivas();
+        
     }
-
-    public void generarSelectedCateogia(int idCategoria) {
-        for (Categoria categoria : categorias) {
-            if (idCategoria == categoria.getIdCategoria()) {
-                selectedCategoria = categoria;
-                break;
-            }
-        }
-    }
-
-    public void saveOrden() {
+    
+    public void saveOrden(){
+        manejadorOrden.Actualizar(selectedOrden);
         RequestContext context = RequestContext.getCurrentInstance();
-        FacesContext context2 = FacesContext.getCurrentInstance();
-        try {
-            manejadorOrden.Actualizar(selectedOrden);
-            context.execute("PF('modificarDialog').hide();");
-            context2.addMessage(null, new FacesMessage("Successful", "Orden Modificada"));
-        } catch (ErrorAplicacion e) {
-            context2.addMessage(null, new FacesMessage("ERROR", "Orden Modificada"));
-        }
+        context.execute("PF('modificarOrdenDialog').hide();");        
+    }
+    
+    public void logDatos(){
+        System.out.println("Manejador: "+manejadorOrden);
+        System.out.println("ordenes: "+ordenes);
+        System.out.println("selectedOrden: "+selectedOrden);
+    }
+    
+    
+    //GETTERS Y SETTERS
+    public List<Orden> getOrdenes() {
+        return ordenes;
     }
 
-    public void saveDetalleOrden() {
-        RequestContext context = RequestContext.getCurrentInstance();
-        //FacesContext context2 = FacesContext.getCurrentInstance();
-        boolean exits = false;
-        for (DetalleOrden detOrd : selectedOrden.getDetalleOrdenList()) {
-            if (detOrd.getProducto().idProducto.equals(selectedProducto.idProducto)) {
-                exits = true;
-                break;
-            }
-        }
-        if (exits) {//Sumarle la cantidad de productos
-            System.out.println("Existe");
-            List<DetalleOrden> l = selectedOrden.getDetalleOrdenList();
-            for (DetalleOrden detOrd : l) {
-                if (detOrd.getProducto().idProducto.equals(selectedProducto.idProducto)) {
-                    //BigDecimal v = BigDecimal.valueOf(cantidadSelectedProducto+Integer.parseInt(detOrd.getCantidad()+""));
-                    int v = cantidadSelectedProducto + detOrd.getCantidad().intValue();
-                    double vd = (double) v;
-                    System.out.println("Nueva Cantidad = "+v);
-                    detOrd.setCantidad(new BigDecimal(vd));
-                    break;
-                }
-            }
-            selectedOrden.setDetalleOrdenList(l);
-            manejadorOrden.Actualizar(selectedOrden);
-        } else {//Crear el Detalle de la Orden
-            System.out.println("Agregar");
-            DetalleOrdenPK detOrdPri = new DetalleOrdenPK(selectedOrden.idOrden, selectedProducto.idProducto);            
-            DetalleOrden detOrd = new DetalleOrden(detOrdPri, new BigDecimal((double) cantidadSelectedProducto));
-            detOrd.setProducto(selectedProducto);
-            detOrd.setOrden(selectedOrden);
-            selectedOrden.getDetalleOrdenList().add(detOrd);
-            manejadorOrden.Actualizar(selectedOrden);
-        }
-        context.execute("PF('agregarProductoDialog').hide();");        
-    }
-
-    public MenuModel getModel() {
-        return model;
-    }
-    
-    public void imprimirCocina(){
-        ticket.TicketCocina(selectedOrden);
-    }
-    
-    public void imprimirBebida(){
-        ticket.TicketBebida(selectedOrden);
-    }
-    
-    public void imprimirVenta(){
-        ticket.TicketVenta(selectedOrden);
-    }
-    
-    public void calcularTotal(){
-        selectedOrden.CalcularTotal();
-    }
-
-    //ALL GETTERS AND SETTERS
     public void setOrdenes(List<Orden> ordenes) {
         this.ordenes = ordenes;
-    }
-
-    public List<Orden> getOrdenes() {
-        return manejadorOrden.ObtenerActivas();
-    }
-
-    public void setSelectedOrden(Orden selectedOrden) {
-        this.selectedOrden = selectedOrden;
     }
 
     public Orden getSelectedOrden() {
         return selectedOrden;
     }
 
-    public List<DetalleOrden> getDetalleOrden() {
-        return selectedOrden.getDetalleOrdenList();
+    public void setSelectedOrden(Orden selectedOrden) {
+        this.selectedOrden = selectedOrden;
+    }
+    
+    public DetalleOrden getSelectedDetalleOrden() {
+        return selectedDetalleOrden;
     }
 
-    public void setDetalleOrden(List<DetalleOrden> detalleOrden) {
-        this.selectedOrden.setDetalleOrdenList(detalleOrden);
-    }
-
-    public Categoria getSelectedCategoria() {
-        return selectedCategoria;
-    }
-
-    public void setSelectedCategoria(Categoria selectedCategoria) {
-        this.selectedCategoria = selectedCategoria;
-    }
-
-    public Producto getSelectedProducto() {
-        return selectedProducto;
-    }
-
-    public void setSelectedProducto(Producto selectedProducto) {
-        this.selectedProducto = selectedProducto;
-    }
-
-    public int getCantidadSelectedProducto() {
-        return cantidadSelectedProducto;
-    }
-
-    public void setCantidadSelectedProducto(int cantidadSelectedProducto) {
-        this.cantidadSelectedProducto = cantidadSelectedProducto;
+    public void setSelectedDetalleOrden(DetalleOrden selectedDetalleOrden) {
+        this.selectedDetalleOrden = selectedDetalleOrden;
     }
 
 }
