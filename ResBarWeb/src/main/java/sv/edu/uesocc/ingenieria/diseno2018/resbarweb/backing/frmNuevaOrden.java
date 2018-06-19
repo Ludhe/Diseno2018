54a1d7f0fcc922a5e541cde0d96012cdafc5d545
juanpc13 <juanpc13lolol@gmail.com>
2018-06-18 18:30:36 -0600

    Agregando cambios de nueva orden

--- ResBarWeb/src/main/java/sv/edu/uesocc/ingenieria/diseno2018/resbarweb/backing/frmNuevaOrden.java
+++ ResBarWeb/src/main/java/sv/edu/uesocc/ingenieria/diseno2018/resbarweb/backing/frmNuevaOrden.java
@@ -16,7 +16,13 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import org.primefaces.context.RequestContext;
+import org.primefaces.model.menu.DefaultMenuItem;
+import org.primefaces.model.menu.DefaultMenuModel;
+import org.primefaces.model.menu.DefaultSubMenu;
+import org.primefaces.model.menu.MenuModel;
+import sv.edu.diseno.acceso.ManejadorCategorias;
 import sv.edu.diseno.acceso.ManejadorOrden;
+import sv.edu.diseno.definiciones.Categoria;
 import sv.edu.diseno.definiciones.DetalleOrden;
 import sv.edu.diseno.definiciones.Orden;
 import sv.edu.diseno.excepciones.ErrorAplicacion;
@@ -28,12 +34,15 @@ import sv.edu.diseno.excepciones.ErrorAplicacion;
 @ManagedBean
 @ViewScoped
 public class frmNuevaOrden implements Serializable {
-    
+
     //Instanciamos los manejadores
     ManejadorOrden manejadorOrden;
+    private List<Categoria> categorias;
+    ManejadorCategorias manejadorCategorias;
     private Orden nuevaOrden;
     Date fecha = new Date();
-    List <DetalleOrden> productos = new ArrayList<>();
+    List<DetalleOrden> productos = new ArrayList<>();
+    private MenuModel model;
 
     public Orden getNuevaOrden() {
         return nuevaOrden;
@@ -42,16 +51,27 @@ public class frmNuevaOrden implements Serializable {
     public void setNuevaOrden(Orden nuevaOrden) {
         this.nuevaOrden = nuevaOrden;
     }
-    
+
     //Para que se ejecute al inicio
     @PostConstruct
     public void init() {
+        model = new DefaultMenuModel();
+        categorias = manejadorCategorias.Obtener(true);
         nuevaOrden = new Orden();
         nuevaOrden.idOrden = manejadorOrden.ObtenerId();
+        DefaultSubMenu firstSubmenu = new DefaultSubMenu("CATEGORÍAS");
+        for (int i = 0; i < categorias.size(); i++) {
+            DefaultMenuItem item = new DefaultMenuItem(categorias.get(i).nombre);
+            item.setIcon("ui-icon-arrowthick-1-e");
+            //item.setCommand("#{administrar.setIdCategoria("+i+")}");
+            item.setCommand("#{frmDashboard.generarSelectedCateogia(" + categorias.get(i).getIdCategoria() + ")}");
+            item.setUpdate(":form:formProductosPanel");
+            firstSubmenu.addElement(item);
+        }
+        model.addElement(firstSubmenu);
     }
-    
-    //getter y setter de las propiedades de la orden
 
+    //getter y setter de las propiedades de la orden
     public int getIdOrden() {
         return nuevaOrden.idOrden;
     }
@@ -95,13 +115,13 @@ public class frmNuevaOrden implements Serializable {
     //para hacer post
     public void save() {
         RequestContext context = RequestContext.getCurrentInstance();
-        try {          
+        try {
             nuevaOrden.activa = true;
             nuevaOrden.fecha = fecha;
             nuevaOrden.detalleOrdenList = productos;
-            nuevaOrden.total = BigDecimal.valueOf(0);       
+            nuevaOrden.total = BigDecimal.valueOf(0);
             manejadorOrden.Insertar(nuevaOrden);
-            context.execute("PF('dialogProductos').show();"); 
+            context.execute("PF('modificarProductoDialog').show();");
         } catch (ErrorAplicacion e) {
 
         }
