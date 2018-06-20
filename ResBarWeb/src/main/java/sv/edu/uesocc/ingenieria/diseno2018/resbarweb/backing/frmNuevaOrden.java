2e951b41214c4c84d819413d09731b5df4addd0d
juanpc13 <juanpc13lolol@gmail.com>
2018-06-20 16:10:07 -0600

    Finalizando cambios del nueva orden

--- ResBarWeb/src/main/java/sv/edu/uesocc/ingenieria/diseno2018/resbarweb/backing/frmNuevaOrden.java
+++ ResBarWeb/src/main/java/sv/edu/uesocc/ingenieria/diseno2018/resbarweb/backing/frmNuevaOrden.java
@@ -6,8 +6,8 @@
 package sv.edu.uesocc.ingenieria.diseno2018.resbarweb.backing;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
@@ -21,9 +21,9 @@ import sv.edu.diseno.acceso.ManejadorCategorias;
 import sv.edu.diseno.acceso.ManejadorOrden;
 import sv.edu.diseno.definiciones.Categoria;
 import sv.edu.diseno.definiciones.DetalleOrden;
+import sv.edu.diseno.definiciones.DetalleOrdenPK;
 import sv.edu.diseno.definiciones.Orden;
 import sv.edu.diseno.definiciones.Producto;
-import sv.edu.diseno.excepciones.ErrorAplicacion;
 import sv.edu.uesocc.ingenieria.diseno2018.resbarweb.ticket.NuevoTicket;
 
 /**
@@ -35,15 +35,129 @@ import sv.edu.uesocc.ingenieria.diseno2018.resbarweb.ticket.NuevoTicket;
 public class frmNuevaOrden implements Serializable {
 
     ManejadorOrden manejadorOrden;
+    private Orden nuevaOrden;
+
     ManejadorCategorias manejadorCategorias;
     private List<Categoria> categorias;
     private Categoria selectedCategoria;
-    private Orden nuevaOrden;
-    private List<Producto> productList;
+
     private Producto selectedProducto;
+    private int cantidadProducto = 1;
+    private DetalleOrden selectedDetalleOrden;
 
     private MenuModel model;
 
+    //Metodos De la Importantes del FRM
+    @PostConstruct
+    public void init() {
+        nuevaOrden = new Orden();
+        nuevaOrden.setIdOrden(manejadorOrden.ObtenerId());
+
+        cantidadProducto = 1;
+        categorias = manejadorCategorias.Obtener(true);
+
+        model = new DefaultMenuModel();
+        DefaultSubMenu firstSubmenu = new DefaultSubMenu("CATEGORÍAS");
+        for (Categoria categoria : categorias) {
+            DefaultMenuItem item = new DefaultMenuItem(categoria.getNombre());
+            item.setCommand("#{frmNuevaOrden.selectCategoria(" + categoria.getIdCategoria() + ")}");
+            item.setUpdate("productosTabla");
+            firstSubmenu.addElement(item);
+        }
+        model.addElement(firstSubmenu);
+    }
+
+    public void saveDetalleOrden() {
+        RequestContext context = RequestContext.getCurrentInstance();
+        //FacesContext context2 = FacesContext.getCurrentInstance();
+        List<DetalleOrden> list = nuevaOrden.getDetalleOrdenList();
+
+        boolean exist = false;
+        for (DetalleOrden detOrd : list) {
+            if (selectedDetalleOrden.getOrden().getIdOrden().equals(detOrd.getOrden().getIdOrden())
+                    && selectedDetalleOrden.getProducto().getIdProducto().equals(detOrd.getProducto().getIdProducto())) {
+                //Producto ya exisite entonces solo sumar el nuevo valor
+                int v = selectedDetalleOrden.getCantidad().intValue() + detOrd.getCantidad().intValue();
+                detOrd.setCantidad(new BigDecimal((double) v));
+                //manejadorOrden.Actualizar(selectedOrden);
+                exist = true;
+                break;
+            }
+
+        }
+
+        if (!exist) {
+            //Creando el Detalle Orden
+            BigDecimal cantidad = new BigDecimal(cantidadProducto);
+            Orden ord = nuevaOrden;
+            Producto pro = selectedProducto;
+            DetalleOrdenPK detOrdPri = new DetalleOrdenPK(ord.idOrden, pro.idProducto);
+            DetalleOrden detOrd = new DetalleOrden(detOrdPri, cantidad);
+            detOrd.setProducto(pro);
+            detOrd.setOrden(ord);
+            list.add(detOrd);
+            nuevaOrden.setDetalleOrdenList(list);
+        }
+
+        nuevaOrden.CalcularTotal();
+        //manejadorOrden.Actualizar(nuevaOrden);        
+        //Mandar a Imprimir la tempList antes de borrarla
+        clearAllSelectionAgregarProducto();
+        context.execute("PF('agregarProductoDialog').hide();");
+    }
+
+    public void guardar() {
+        if (!nuevaOrden.detalleOrdenList.isEmpty()) {
+            manejadorOrden.Insertar(nuevaOrden);
+            NuevoTicket ticket = new NuevoTicket();
+            boolean cocina = false, bebidas = false;
+            for (DetalleOrden detalleOrden : nuevaOrden.detalleOrdenList) {
+                if (detalleOrden.producto.area == 'C') {
+                    cocina = true;
+                }
+                if (detalleOrden.producto.area == 'B') {
+                    bebidas = true;
+                }
+            }
+            if (bebidas) {
+                ticket.TicketExtraBebida(nuevaOrden.detalleOrdenList);
+            }
+            if (cocina) {
+                ticket.TicketExtraCocina(nuevaOrden.detalleOrdenList);
+            }
+        } else {
+            System.out.println("la lista de productos esta vacia");
+        }
+
+    }
+
+    public void clearAllSelectionAgregarProducto() {
+        selectedCategoria = null;
+        selectedProducto = null;
+        cantidadProducto = 1;
+    }
+
+    public void selectCategoria(int idCategoria) {
+        for (Categoria categoria : categorias) {
+            if (idCategoria == categoria.getIdCategoria()) {
+                selectedCategoria = categoria;
+                break;
+            }
+        }
+    }
+
+    public void initDetalleOrden() {
+        if (nuevaOrden.idOrden != null && !nuevaOrden.mesa.isEmpty() || !nuevaOrden.mesero.isEmpty() || !nuevaOrden.cliente.isEmpty()) {
+            this.nuevaOrden.setDetalleOrdenList(new ArrayList<DetalleOrden>());
+            RequestContext context = RequestContext.getCurrentInstance();
+            context.execute("PF('agregarProductoDialog').show();");
+        } else {
+            System.out.println("No se pueden dejar campos vacios");
+        }
+
+    }
+
+    //GETTERS Y SETTER PARA NUEVA ORDEN
     public Orden getNuevaOrden() {
         return nuevaOrden;
     }
@@ -52,14 +166,7 @@ public class frmNuevaOrden implements Serializable {
         this.nuevaOrden = nuevaOrden;
     }
 
-    public List<Producto> getProductList() {
-        return productList;
-    }
-
-    public void setProductList(List<Producto> productList) {
-        this.productList = productList;
-    }
-
+    //GETTERS Y SETTERS DE CATEGORIOAS
     public List<Categoria> getCategorias() {
         return categorias;
     }
@@ -76,16 +183,7 @@ public class frmNuevaOrden implements Serializable {
         this.selectedCategoria = selectedCategoria;
     }
 
-    //GETTERS Y SETTERS DEL PRODUCTO
-    public Producto getSelectedProducto() {
-        return selectedProducto;
-    }
-
-    public void setSelectedProducto(Producto selectedProducto) {
-        this.selectedProducto = selectedProducto;
-    }
-
-    //GETTERS Y SETTERS DEL MODELO
+    //GETTERS Y SETTER DEL MODELO MENU
     public MenuModel getModel() {
         return model;
     }
@@ -94,57 +192,30 @@ public class frmNuevaOrden implements Serializable {
         this.model = model;
     }
 
-    @PostConstruct
-    public void init() {
-        categorias = new ArrayList<>();
-        categorias = manejadorCategorias.Obtener(true);
-        selectedProducto = new Producto();
-        model = new DefaultMenuModel();
-        nuevaOrden = new Orden();
-        nuevaOrden.idOrden = ManejadorOrden.ObtenerId();
-        DefaultSubMenu firstSubmenu = new DefaultSubMenu("CATEGORÍAS");
-        for (Categoria categoria : categorias) {
-            DefaultMenuItem item = new DefaultMenuItem(categoria.getNombre());
-            item.setCommand("#{frmDashboard.selectCategoria(" + categoria.getIdCategoria() + ")}");
-            item.setUpdate("productosTabla");
-            firstSubmenu.addElement(item);
-        }
-        model.addElement(firstSubmenu);
+    //GETTERS Y SETTER DEL PRODUCTO SELECIONADO
+    public Producto getSelectedProducto() {
+        return selectedProducto;
     }
 
-    //para hacer pasar a agregar productos
-    public void productos() {
-        if ((!nuevaOrden.cliente.isEmpty()) || (!nuevaOrden.mesa.isEmpty()) || (!nuevaOrden.mesero.isEmpty())) {
-            System.out.println("LLEGUE CON ALGUNO NO VACIO");
-            RequestContext context = RequestContext.getCurrentInstance();
-            context.execute("PF('addProductos').show();");
-        }
-
+    public void setSelectedProducto(Producto selectedProducto) {
+        this.selectedProducto = selectedProducto;
     }
 
-    public void addProductos() {
-        RequestContext context = RequestContext.getCurrentInstance();
-        //no ponerlo vacio --->  nuevaOrden.detalleOrdenList = productos; 
-        context.execute("PF('addProductos').hide();");
+    //GETTERS Y SETTER DE LA CANTIDAD
+    public int getCantidadProducto() {
+        return cantidadProducto;
     }
 
-    public void cancelar() {
-        RequestContext context = RequestContext.getCurrentInstance();
-        context.execute("PF('addProductos').hide();");
+    public void setCantidadProducto(int cantidadProducto) {
+        this.cantidadProducto = cantidadProducto;
     }
 
-    //para crear la orden
-    public void guardar() {
-        try {
-            nuevaOrden.activa = true;
-            nuevaOrden.CalcularTotal();
-            ManejadorOrden.Insertar(nuevaOrden);
-            //si se crea la nueva orden se imprimen los tickets automaticos
-            // ticket.TicketBebida(nuevaOrden);
-            // ticket.TicketCocina(nuevaOrden);
-        } catch (Exception e) {
+    public DetalleOrden getSelectedDetalleOrden() {
+        return selectedDetalleOrden;
+    }
 
-        }
+    public void setSelectedDetalleOrden(DetalleOrden selectedDetalleOrden) {
+        this.selectedDetalleOrden = selectedDetalleOrden;
     }
 
 }
